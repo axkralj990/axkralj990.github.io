d08ca635da79d6fe8eed915b2cf8cd626a6987b3
axkralj990 <alex.kraljic@gmail.com>
2017-09-15 17:42:47 +0200

    new menu added

--- myNavbar.js
+++ myNavbar.js
@@ -3,6 +3,7 @@ var x = document.getElementById("myNav");
 activateHome = "";
 activateAbout = "";
 activateProjects = "";
+activateTutorials = "";
 if (x.classList.contains("home")) {
 	activateHome = "active";
 }
@@ -11,6 +12,9 @@ else if (x.classList.contains("about")) {
 }
 else if (x.classList.contains("projects")) {
 	activateProjects = "active";
+}
+else if (x.classList.contains("tutorials")) {
+    activateTutorials = "active";
 }
 
 x.innerHTML = 
@@ -23,21 +27,27 @@ x.innerHTML =
                 '<span class="icon-bar"></span>'+
                 '<span class="icon-bar"></span>'+
                 '</button>'+
-            	'<a href="index.html" class="navbar-brand" id="AK-brand">A-K</a>'+
+            	'<a href="index.html" class="navbar-brand" id="AK-brand">AK</a>'+
             '</div>'+
             '<!-- menu -->'+
             '<div class="collapse navbar-collapse" id="mainNavBar">'+
             	'<ul class="nav navbar-nav">'+
                 	'<li class="' + activateHome + '"><a href="index.html">Home</a></li>'+
                     	'<li class="' + activateAbout + '"><a href="about_me.html">About Me</a></li>'+
-                    	'<!-- drop down menu -->'+
+                        '<li class="dropdown ' + activateTutorials + '">'+
+                            '<a href="#" class="dropdown-toggle" data-toggle="dropdown">Tutorials<span class="caret"></span></a>'+
+                            '<ul class="dropdown-menu">'+
+                                '<li><a href="I2C.html">I2C Exaplained</a></li>'+
+                                '<li><a href="MPU6050.html">MPU6050</a></li>'+
+                                '<li><a href="MRI.html">MRI Imaging</a></li>'+
+                                '<li><a href="MRI.html">EEG</a></li>'+
+                            '</ul>'+
+                        '</li>'+
                     	'<li class="dropdown ' + activateProjects + '">'+
                     		'<a href="#" class="dropdown-toggle" data-toggle="dropdown">Projects<span class="caret"></span></a>'+
                         	'<ul class="dropdown-menu">'+
                         		'<li><a href="quad.html">ATmega Quadcopter</a></li>'+
-                   			'<li><a href="SGM_cs.html">Serial Grahing Monitor - C#</a></li>'+
-                            		'<li><a href="SGM_python.html">Serial Graphing Monitor - Python</a></li>'+
-                   			'<li><a href="AVR_misc.html">Smaller AVR Projects</a></li>'+
+                   			    '<li><a href="SGM.html">Serial Grahing Monitor</a></li>'+
                         	'</ul>'+
                     	'</li>'+
 			'<li><a href="https://github.com/axkralj990">GitHub Page</a></li>'+
